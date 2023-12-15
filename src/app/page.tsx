f65d70af9c1118d10efd78ceb8065d31ee1999c5
Pinapelz <yukais@pinapelz.com>
2023-12-15 11:00:03 -0800

    change graph url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import SubscriberTable, {SubscriberDataTableProp} from '../components/Subscriber
 import TitleBar from '../components/TitleBar/TitleBar';
 
 async function Home(){
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL_TESTING
+    const graphURL = process.env.NEXT_PUBLIC_GRAPH_URL
     const data: SubscriberDataTableProp = await getData();
     return(
         <>
             <TitleBar title="PhaseTracker" backgroundColor='black' />
             <div className="sm:block hidden mt-4" style={{ overflow: 'hidden', height: '105vh', position: 'relative' }}>
-                <iframe src={apiUrl} style={{ position: 'absolute', top: 0, left: 0 }} width="100%" height="100%"></iframe>
+                <iframe src={graphURL} style={{ position: 'absolute', top: 0, left: 0 }} width="100%" height="100%"></iframe>
             </div>
             <SubscriberTable {...data} />
         </>
