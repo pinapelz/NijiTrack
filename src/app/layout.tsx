bd466033e899ef8f1684057b443300fc6c61eed6
Pinapelz <yukais@pinapelz.com>
2024-10-04 00:52:56 -0700

    remove unused Inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import Head from "next/head";
 import Footer from "../components/Footer/Footer";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "PhaseTracker - Phase Connect Subscriber Tracker",
@@ -20,7 +17,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
+			<body>
 				{children}
 				<Footer />
 				<Analytics />
