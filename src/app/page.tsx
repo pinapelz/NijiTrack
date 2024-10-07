e23005df1e1320729589091f7a2547aa6fac593a
Pinapelz <yukais@pinapelz.com>
2024-10-07 10:58:49 -0700

    fix some biomejs lint issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,46 @@
 import SubscriberTable, {
-	type SubscriberDataTableProp,
+  type SubscriberDataTableProp,
 } from "../components/SubscriberTable/SubscriberTable";
 import TitleBar from "../components/TitleBar/TitleBar";
 
 async function Home() {
-	const graphURL = process.env.NEXT_PUBLIC_GRAPH_URL;
-	const data: SubscriberDataTableProp = await getData();
-	return (
-		<>
-			<TitleBar title="PhaseTracker" backgroundColor="black" />
-			<div
-				className="sm:block hidden mt-4"
-				style={{ overflow: "hidden", height: "105vh", position: "relative" }}
-			>
-				<iframe
-					src={graphURL}
-					style={{ position: "absolute", top: 0, left: 0 }}
-					width="100%"
-					height="100%"
-				></iframe>
-			</div>
-			<SubscriberTable {...data} />
-		</>
-	);
+  const graphURL = process.env.NEXT_PUBLIC_GRAPH_URL;
+  const data: SubscriberDataTableProp = await getData();
+  return (
+    <>
+      <TitleBar title="PhaseTracker" backgroundColor="black" />
+      <div
+        className="sm:block hidden mt-4"
+        style={{ overflow: "hidden", height: "105vh", position: "relative" }}
+      >
+        <iframe
+          title="Phase Connect Subscriber Count Graph"
+          src={graphURL}
+          style={{ position: "absolute", top: 0, left: 0 }}
+          width="100%"
+          height="100%"
+        />
+      </div>
+      <SubscriberTable {...data} />
+    </>
+  );
 }
 
 async function getData() {
-	const apiUrl = process.env.NEXT_PUBLIC_API_URL_TESTING;
-	const response = await fetch(apiUrl + "/api/subscribers", {
-		headers: {
-			"Cache-Control": "no-cache",
-		},
-		cache: "no-cache",
-	});
-	if (!response.ok) {
-		console.log(response.statusText);
-	}
-	return response.json();
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL_TESTING;
+  const endpoint = "/api/subscribers";
+  const headers = {
+    "Cache-Control": "no-cache",
+  };
+  const cacheOption = "no-cache";
+
+  const response = await fetch(`${apiUrl}${endpoint}`, {
+    headers: headers,
+    cache: cacheOption,
+  });
+  if (!response.ok) {
+    console.log(response.statusText);
+  }
+  return response.json();
 }
 export default Home;
