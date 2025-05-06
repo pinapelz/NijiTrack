65d724700cf777253e808415bc22dc54edf56dde
Pinapelz <yukais@pinapelz.com>
2025-05-06 02:41:05 -0700

    fix text spacing on mobile table

--- src/components/SubscriberTable/SubscriberTableRow.tsx
+++ src/components/SubscriberTable/SubscriberTableRow.tsx
@@ -14,17 +14,17 @@ const ChannelRow: React.FC<ChannelRowProps> = ({ channel, index }) => (
 		className="border-b hover:bg-gray-100 cursor-pointer"
 		onClick={() => (window.location.href = "/stats/" + channel.channel_name)}
 	>
-		<td className="py-3 px-1 sm:px-3 hidden sm:table-cell">{index + 1}</td>
-		<td className="py-3 px-1 sm:px-3 flex items-center">
-			<Image
-				src={channel.profile_pic}
-				alt={channel.channel_name}
-				width={50}
-				height={50}
-				className="rounded-full"
-			/>
-			<span className="ml-2">{channel.channel_name}</span>
-		</td>
+	<td className="py-3 px-1 sm:px-3 hidden sm:table-cell">{index + 1}</td>
+	<td className="py-3 px-1 sm:px-3 flex items-center">
+		<Image
+			src={channel.profile_pic}
+			alt={channel.channel_name}
+			width={50}
+			height={50}
+			className="rounded-full"
+		/>
+		<span className="ml-2">{channel.channel_name}</span>
+	</td>
 		<td className="py-3 px-1 sm:px-3 hidden sm:table-cell">
 			{channel.sub_org}
 		</td>
