cf8d330798ed4f759d9a75199cac094d3eb1f496
Pinapelz <yukais@pinapelz.com>
2025-10-18 20:42:31 -0700

    fix layout metadata component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
-import type { Metadata } from "next";
+import Metadata from "next";
 import Footer from "../components/Footer/Footer";
 import "./globals.css";
 
